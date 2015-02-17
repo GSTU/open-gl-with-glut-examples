d6c216562ad48d319dd500231af861c277fde370
Igor Nesterenya <nesterione@gmail.com>
2015-02-17 20:42:14 +0300

    Change script

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
 
 
-gcc main.c -o main -lopengl32 -lglu32 -I"%~dp0/include" -L"%~dp0/lib" -lglut32
+gcc main.c -o main -lopengl32 -lglu32 -I"%~dp0/include" -L"%~dp0/lib" -lglut32 && main.exe
 pause
